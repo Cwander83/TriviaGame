17e5c7eb264e9ab2356d30c82e0a137deb6802ef
Cwander83 <chriswandermail@gmail.com>
2018-01-20 00:01:25 -0500

    1-19-18(working on Q and A)

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,7 @@
 //starting with a array 
 //=========================================================
 //JavaScript Page for a shark base trivia game
-var panel = $("#quiz-area");
+
 
 var questions = [{
         //1 great white
@@ -54,6 +54,12 @@ var questions = [{
     }
 ];
 
+
+
+//vars i think i will use
+//==============================================
+
+var panel = $("#quiz-area");
 var timer;
 var countStartNumber = 60;
 
@@ -64,9 +70,24 @@ var game = {
     correct: 0,
     incorrect: 0,
 
-    countdown: function () {},
+    countdown: function () {
+        this.counter--;
+        $("#counter-number").text(this.counter);
+        if (this.counter === 0) {
+            console.log("Time Up!");
+            this.timeUp();
+        }
+    },
 
-    loadQuestion: function () {},
+    loadQuestion: function () {
+        timer = setInterval(this.countdown.bind(this), 1000);
+        panel.html("<h2>" + questions[this.currentQuestion].question + "</h2>");
+
+        for (var i = 0; i < questions[this.currentQuestion].answers.length; i++) {
+            panel.append("<button class='btn btn-info' id='button' data-name='" + questions[this.currentQuestion].answers[i] +
+                "'>" + questions[this.currentQuestion].answers[i] + "</button>");
+        }
+    },
 
     nextQuestion: function () {},
 
@@ -82,70 +103,3 @@ var game = {
 
     reset: function () {}
 };
-
-
-
-
-
-//vars i think i will use
-//===============================================
-
-
-var wrongCount = 0;
-console.log("wrongcount: " + wrongCount);
-var rightCount = 0;
-console.log("rightcount: " + rightCount);
-
-var questionArray = [questionOne.question, questionTwo.question, questionThree.question, questionFour.question, questionFive.question, questionSix.question, questionSeven.question];
-
-console.log(questionArray[2]);
-
-
-for (var i = 0; i < questionArray.length; i++) {
-    questions = questionArray + i;
-    console.log("array" + questions[i]);
-}
-
-
-
-//functions i think i will use
-//===============================================
-
-function displayVidImg() {
-    $(".imgvid").attr("src", questionOne.video);
-}
-//
-//function nextQuestion()
-function startgame() {
-
-    $("#questionid").append(questionOne.question);
-    $("#a1").append(questionOne.a[0]);
-    $("#a2").append(questionOne.b[0]);
-    $("#a3").append(questionOne.c[0]);
-    $("#a4").append(questionOne.d[0]);
-
-    //$("#sharkvideo").load(questionOne.video);
-
-    $(".answerslist").on("click", function () {
-
-        if (questionOne.b[1] === true) {
-            $("#a1, #a3, #a4").hide(1000);
-            $("#a2").css("color", "blue");
-            rightCount++;
-            alert("yes");
-            console.log(this);
-
-        }
-        /*else if (this === false)
-            console.log("else" + this);
-        alert("no wrong")
-        wrongCount++;
-        */
-    });
-}
-$(document).ready(function () {
-    setTimeout(startgame, 2000 * 1);
-    setTimeout(displayVidImg, 3000 * 1);
-
-});
-//function reset(){}
